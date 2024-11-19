1360ee0a12c9b616e10c5d83e127e57d45c0e7c9
satyaki-mitra <satyaki@itobuz.com>
2024-11-19 15:27:20 +0530

    project folders structures updated

--- Project_1_2/config.py
+++
@@ -1,5 +0,0 @@
-# ALL DATAFILE PATHS
-ORDERS_CSV = './data/orders.csv' 
-PRODUCTS_CSV = './data/products.csv'
-ORDER_PRODUCTS_CSV = './data/order_products.csv'
-AISLE_CSV = './data/aisles.csv'
--- Project_1_2/src/utils.py
+++
@@ -1,7 +0,0 @@
-
-
-def encode_units(x):
-        if x <= 0:
-            return 0
-        if x >= 1:
-            return 1
