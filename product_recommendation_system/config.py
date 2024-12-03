7bdd9141dc30b1aec0b67c064f4b91fbef467b55
Amrit-Itobuz <amrit@itobuz.com>
2024-12-03 17:40:25 +0530

    product_recommendation_system

--- product_recommendation_system/config.py
+++ product_recommendation_system/config.py
@@ -7,7 +7,14 @@ departments    = "../data/departments.csv"
 
 
 frac           = 0.1
+random_state   = 42
+target_column  = 'reordered'
+data_file_path = 'data/hyperparameter_tuning_dataset.csv'
 
 json_path      = './plots/EDA/test_results.json'
 
 file_loc       = "./plots/EDA/"
+
+stratified_sample_data = ''
+hyperparameter_tuning_total_results = 'results/hyperparameter_tuning_total_rersults.json'
+hyperparameter_tuning_top5_results  = 'results/hyperparameter_tuning_top5_rersults.csv'
