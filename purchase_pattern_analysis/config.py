7cb2143abe8078c3bc7c342715b623d7628871f3
avantika <avantika@itobuz.com>
2024-12-04 15:56:48 +0530

    Synced config with the global config.py file for purchase_pattern_analysis by Avantika

--- purchase_pattern_analysis/config.py
+++ purchase_pattern_analysis/config.py
@@ -1,5 +1,11 @@
 # ALL DATAFILE PATHS
-ORDERS_CSV = './data/orders.csv' 
-PRODUCTS_CSV = './data/products.csv'
-ORDER_PRODUCTS_CSV = './data/order_products.csv'
-AISLE_CSV = './data/aisles.csv'
+ORDERS_CSV          = '../data/orders.csv' 
+PRODUCTS_CSV        = '../data/products.csv'
+ORDER_PRODUCTS_CSV  = '../data/order_products.csv'
+AISLE_CSV           = '../data/aisles.csv'
+ENCODED_MERGED_DATA = '../data/encoded_data.csv'
+PROCESSED_ORDERS    = '../data/orders_processed.csv'
+
+
+# GLOBAL VARIABLES
+RANDOM_STATE        = 42
--- purchase_pattern_analysis/src/data_description.py
+++ purchase_pattern_analysis/src/data_description.py
@@ -2,17 +2,29 @@
 import pandas as pd
 
 def data_head(dataframe: pd.DataFrame):
-    """Display the first 5 rows of the dataset"""
-    return dataframe.head()
+    try:
+        """Display the first 5 rows of the dataset"""
+        return dataframe.head()
+    except Exception as unable_to_check_head:
+        return repr(unable_to_check_head)
 
 def data_unique(dataframe: pd.DataFrame):
-    """Calculating total unique values for each column"""
-    return dataframe.nunique()
+    try:
+        """Calculating total unique values for each column"""
+        return dataframe.nunique()
+    except Exception as unable_to_check_nunique:
+        return repr(unable_to_check_nunique)
 
 def data_null(dataframe: pd.DataFrame):
-    """Calculating the total missing values for each column"""
-    return dataframe.isna().sum()
+    try:
+        """Calculating the total missing values for each column"""
+        return dataframe.isna().sum()
+    except Exception as unable_to_check_null:
+        return repr(unable_to_check_null)
 
 def data_duplicate(dataframe: pd.DataFrame):
-    """Calculating if there is any duplicate values in the dataset"""
-    return dataframe.duplicated().sum()
+    try:
+        """Calculating if there is any duplicate values in the dataset"""
+        return dataframe.duplicated().sum()
+    except Exception as unable_to_check_duplicate:
+        return repr(unable_to_check_duplicate)
