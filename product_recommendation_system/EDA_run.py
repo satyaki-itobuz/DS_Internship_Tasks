e8374c5f9702b9fb0684a89959d52cdc8315ed89
Mik-itobuz <soumik@itobuz.com>
2024-12-04 16:22:51 +0530

    updated dependencies

--- product_recommendation_system/EDA_run.py
+++ product_recommendation_system/EDA_run.py
@@ -1,7 +1,27 @@
-from src.EDA_data_preprocessing import *
-from src.eda import *
-from config import *
-from src.utils import *
+from src.EDA_data_preprocessing import clean_data,missing_values
+from src.eda import plot_histogram
+from src.eda import plot_boxplot
+from src.eda import plot_correlation_matrix
+from src.eda import stratified_sample
+from src.eda import skew_kurtosis
+from src.eda import pie_chart
+from src.eda import bar_plot
+from src.eda import box_plot_bivariate
+from src.eda import cross_tab_heatmap
+from src.eda import plot_reorder_rate_by_category
+from src.eda import plot_mean_add_to_cart_order_by_hour
+from src.eda import plot_reorder_rate_heatmap
+from src.eda import analyze_reorder_probability
+from src.eda import countplot,apply_binning
+from src.eda import run_statistical_tests
+from config import ORDER
+from config import ORDER_PRODUCTS
+from config import DEPARTMENTS
+from config import PRODUCTS 
+from config import AISLES 
+from config import STRATIFICATION_FRACTION
+from config import EDA_TEST_RESULTS
+from src.utils import load_data,merging_data
 from datetime import datetime
 import os
 import logging
@@ -30,41 +50,41 @@ dept=load_data(DEPARTMENTS)
 
 # merging data 
 logging.info("Merging data")
-pro1=merging_data(df1=pro,df2=dept,on_column='department_id')
-pro2=merging_data(df1=pro1,df2=aisles,on_column='aisle_id')
-pro3=merging_data(df1=pro2,df2=order_pro,on_column='product_id')
-df=merging_data(df1=pro3,df2=orders,on_column='order_id')
+products1=merging_data(df1=pro,df2=dept,on_column='department_id')
+products2=merging_data(df1=products1,df2=aisles,on_column='aisle_id')
+products3=merging_data(df1=products2,df2=order_pro,on_column='product_id')
+merged_df=merging_data(df1=products3,ductsdf2=orders,on_column='order_id')
 
 # cleaning data
 logging.info("Cleaning data")
-df=clean_data(df=df,col='eval_set')
+merged_df=clean_data(df=merged_df,col='eval_set')
 
 # missing value treatment
 logging.info("Missing value treatment")
-df=missing_values(df=df,col='days_since_prior_order')
+merged_df=missing_values(df=merged_df,col='days_since_prior_order')
 
 # stratified sampling 
 logging.info("Sampling performed")
-df=stratified_sample(df=df,stratify_col='product_name',frac=STRATIFICATION_FRACTION)
+merged_df=stratified_sample(df=merged_df,stratify_col='product_name',frac=STRATIFICATION_FRACTION)
 
 # histogram of numerical columns 
 logging.info("Histogram of numerical columns")
 columns_to_plot = ['add_to_cart_order','order_number','days_since_prior_order','order_hour_of_day'] 
 for i in range(len(columns_to_plot)):
-    plot_histogram(df=df,col=columns_to_plot[i],save=True)
+    plot_histogram(df=merged_df,col=columns_to_plot[i],save=True)
 
 
 # box plot of numerical columns 
 logging.info("box plot of numerical columns")
 cols_to_plot = ['order_hour_of_day','days_since_prior_order'] 
 for i in range(len(cols_to_plot)):
-    plot_boxplot(df=df,col=cols_to_plot[i],save=True)
+    plot_boxplot(df=merged_df,col=cols_to_plot[i],save=True)
 
 # skewness and kurtosis of the numerical columns 
 logging.info("skewness and kurtosis of the numerical columns")
 cols_to_plot_2 = ['add_to_cart_order', 'order_number', 'order_hour_of_day', 'days_since_prior_order', 'order_dow']
 for col in cols_to_plot_2:
-    result = skew_kurtosis(df, col)
+    result = skew_kurtosis(merged_df, col)
     print(f"Skewness and Kurtosis for '{col}': {result}")
 
 
@@ -72,52 +92,52 @@ for col in cols_to_plot_2:
 logging.info("pie chart of the columns")
 pie_columns=['department','aisle','order_dow']
 for col in cols_to_plot_2:
-    pie_chart(df=df,col=col)
+    pie_chart(df=merged_df,col=col)
 
 # bar plot of categorical columns 
 logging.info("bar plot of categorical columns")
 bar_columns=['department','aisle','order_dow']
 for col in bar_columns:
-    bar_plot(df=df,col=col)
+    bar_plot(df=merged_df,col=col)
 
 # box plot for bivariate analysis
 logging.info("bivariate analysis")
-box_plot_bivariate(df=df,col1='department',col2='days_since_prior_order')
-box_plot_bivariate(df=df,col1='aisle',col2='add_to_cart_order')
+box_plot_bivariate(df=merged_df,col1='department',col2='days_since_prior_order')
+box_plot_bivariate(df=merged_df,col1='aisle',col2='add_to_cart_order')
 
 # contigency table with heatmap 
 logging.info("contingency table with heatmap")
-cross_tab_heatmap(df=df,col1='order_hour_of_day',col2='order_dow',save=True)
-cross_tab_heatmap(df=df,col1='department',col2='reordered',save=True)
-cross_tab_heatmap(df=df,col1='order_hour_of_day',col2='department',save=True)
+cross_tab_heatmap(df=merged_df,col1='order_hour_of_day',col2='order_dow',save=True)
+cross_tab_heatmap(df=merged_df,col1='department',col2='reordered',save=True)
+cross_tab_heatmap(df=merged_df,col1='order_hour_of_day',col2='department',save=True)
 
 # reorder rate by category heatmap 
 logging.info("reorder rate by category heatmap")
-plot_reorder_rate_by_category(df=df)
+plot_reorder_rate_by_category(df=merged_df)
 
 # mean of add to cart otder by hour heatmap
-plot_mean_add_to_cart_order_by_hour(df=df) 
+plot_mean_add_to_cart_order_by_hour(df=merged_df) 
 
 # reorder rate heatmap
 logging.info("reorder rate heatmap")
-plot_reorder_rate_heatmap(df=df)
+plot_reorder_rate_heatmap(df=merged_df)
 
 # reorder probability heatmap
 logging.info("reorder probability heatmap")
-analyze_reorder_probability(df=df,feature1='order_dow',feature2='order_hour_of_day')
+analyze_reorder_probability(df=merged_df,feature1='order_dow',feature2='order_hour_of_day')
 
 # binning and bucketing
 logging.info("binning and bucketing")
-stratified_df=apply_binning(stratified_df=df)
+stratified_df=apply_binning(stratified_df=merged_df)
 
 print(stratified_df[['time_of_day', 'customer_type', 'order_recency', 'peak_category']].head())
 
 # countplot of binning and bucketing
 logging.info("countplot of binning and bucketing")
-countplot(df=df,col=stratified_df['time_of_day'])
-countplot(df=df,col=stratified_df['customer_type'])
-countplot(df=df,col=stratified_df['order_recency'])
-countplot(df=df,col=stratified_df['peak_category'])
+countplot(df=merged_df,col=stratified_df['time_of_day'])
+countplot(df=merged_df,col=stratified_df['customer_type'])
+countplot(df=merged_df,col=stratified_df['order_recency'])
+countplot(df=merged_df,col=stratified_df['peak_category'])
 
 
 numerical_cols = ['add_to_cart_order', 'reordered', 'order_number', 
@@ -125,7 +145,7 @@ numerical_cols = ['add_to_cart_order', 'reordered', 'order_number',
 
 # correlation matrix of numerical columns 
 logging.info("correlation matrix of numerical columns")
-plot_correlation_matrix(df, numerical_cols)
+plot_correlation_matrix(merged_df, numerical_cols)
 
 # statistical tests 
 logging.info("statistical tests")
